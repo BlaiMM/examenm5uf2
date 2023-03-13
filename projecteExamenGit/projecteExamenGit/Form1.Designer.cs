c80d5ee9e2a548c24f9bf6a1599a3ca0539afcf9
BlaiMM <blai.menchenm@educem.net>
2023-03-13 08:40:50 +0100

    versió 4

--- projecteExamenGit/projecteExamenGit/Form1.Designer.cs
+++ projecteExamenGit/projecteExamenGit/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace projecteExamenGit
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btAnimal = new System.Windows.Forms.Button();
+            this.btViatge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btAnimal
+            // 
+            this.btAnimal.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btAnimal.ForeColor = System.Drawing.Color.Chocolate;
+            this.btAnimal.Location = new System.Drawing.Point(93, 88);
+            this.btAnimal.Name = "btAnimal";
+            this.btAnimal.Size = new System.Drawing.Size(75, 23);
+            this.btAnimal.TabIndex = 0;
+            this.btAnimal.Text = "Animal";
+            this.btAnimal.UseVisualStyleBackColor = false;
+            // 
+            // btViatge
+            // 
+            this.btViatge.BackColor = System.Drawing.Color.Tomato;
+            this.btViatge.ForeColor = System.Drawing.Color.Gold;
+            this.btViatge.Location = new System.Drawing.Point(195, 88);
+            this.btViatge.Name = "btViatge";
+            this.btViatge.Size = new System.Drawing.Size(75, 23);
+            this.btViatge.TabIndex = 1;
+            this.btViatge.Text = "Viatge";
+            this.btViatge.UseVisualStyleBackColor = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(392, 192);
+            this.Controls.Add(this.btViatge);
+            this.Controls.Add(this.btAnimal);
+            this.Name = "frmMain";
+            this.Text = "main";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btAnimal;
+        private System.Windows.Forms.Button btViatge;
     }
 }
 
